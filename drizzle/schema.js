9af1ebbb28c81792a27533b1d72c73b65e3177eb
elishabhatti <elishakumari2007@gmail.com>
2025-03-25 13:38:40 +0500

    added user schema in drizzle

--- drizzle/schema.js
+++ drizzle/schema.js
@@ -1,7 +1,18 @@
-import { int, mysqlTable, varchar } from "drizzle-orm/mysql-core";
+import { timestamp } from "drizzle-orm/gel-core";
+import { serial, int, mysqlTable, varchar } from "drizzle-orm/mysql-core";
 
 export const shortLink = mysqlTable("short_link", {
   id: int().autoincrement().primaryKey(),
   url: varchar({ length: 255 }).notNull(),
   shortCode: varchar("short_code", { length: 20 }).notNull().unique(),
+});
+
+
+export const users = mysqlTable('users', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 255 }).notNull(),
+  email: varchar('email', { length: 255 }).notNull().unique(),
+  password: varchar('password', { length: 255 }).notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
 });
