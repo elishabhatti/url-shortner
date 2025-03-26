85b0e98948ab29c8fca0430e9bb5d5beb66b407b
elishabhatti <elishakumari2007@gmail.com>
2025-03-26 21:28:38 +0500

    added dry principle in zod schema

--- validators/auth-validator.js
+++ validators/auth-validator.js
@@ -1,24 +1,5 @@
 import z from "zod";
 
-export const registerUserSchema = z.object({
-  name: z
-    .string()
-    .trim()
-    .min(3, { message: "Name must be aleast 3 characters long." })
-    .max(100, { message: "Name must be less then 100 characters." }),
-  email: z
-    .string()
-    .trim()
-    .email()
-    .max(100)
-    .min(3, { message: "Email must be aleast 3 characters long." })
-    .max(100, { message: "Email must be less then 100 characters." }),
-  password: z
-    .string()
-    .max(100)
-    .min(6, { message: "Password must be aleast 3 characters long." })
-    .max(100, { message: "Password must be less then 100 characters." }),
-});
 export const loginUserSchema = z.object({
   email: z
     .string()
@@ -32,4 +13,12 @@ export const loginUserSchema = z.object({
     .max(100)
     .min(6, { message: "Password must be aleast 3 characters long." })
     .max(100, { message: "Password must be less then 100 characters." }),
+});
+
+export const registerUserSchema = loginUserSchema.extend({
+  name: z
+    .string()
+    .trim()
+    .min(3, { message: "Name must be aleast 3 characters long." })
+    .max(100, { message: "Name must be less then 100 characters." }),
 });
